e18f9015ff96b20663a3ced017fc8a3257bbc088
Luis Guillen <guillen@MacBook-Pro-de-Luis.local>
2020-12-17 01:00:42 -0600

    Ultimo archivo

---
+++ Usuario.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang=“es”>
+   <head>
+	<title>  </title>
+   </head>
+   <body>
+	
+		<?php
+			$servidor = 'localhost';	//Servidor
+			$usuario = 'Guillen';	//Usuario
+			$contraseña = '****';	//Contraseña
+			$basedatos = 'registro';	//Base de datos donde estan las tablas
+			$port =	"8889";	//Puerto
+			
+		?>
+	
+   </body>
+ </html>
